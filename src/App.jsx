dfb0da046175ebe9060f47f264d2ea63d3f970da
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-07-14 12:47:43 +0900

    Reactでの実装を意識したモックに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,40 @@
+import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["aaaaa", "bbbbb"]);
+  const [completeTodos, setCompleteTodos] = useState(["ccccc", "ddddd"]);
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
-            <li>aaaa</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div>
-            <li>bbbbb</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
-          <div>
-            <li>cccc</li>
-            <button>戻す</button>
-          </div>
-          <div>
-            <li>dddddd</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
